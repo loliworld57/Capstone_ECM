3e41d802a1f480d80ee5150d06336f1579a77aaf
Tung Hoang <loliworld57@gmail.com>
2026-06-07 16:08:41 +0700

    Security Update

--- backend/src/main/java/com/extracenter/backend/utils/JwtUtils.java
+++ backend/src/main/java/com/extracenter/backend/utils/JwtUtils.java
@@ -1,21 +1,32 @@
 package com.extracenter.backend.utils;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.stereotype.Component;
-import com.extracenter.backend.entity.User;
-
 import java.security.Key;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.extracenter.backend.entity.User;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
 @Component
 public class JwtUtils {
+    
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
 
     // Khóa bí mật (Nên để phức tạp và giấu trong file cấu hình .env)
-    private static final String SECRET_KEY = "DayLaKhoaBiMatCuaECMSystemRatDaiVaPhucTapDeBaoMat123456";
+    private final String SECRET_KEY = secretKey;
 
     // Thời gian hết hạn: 24 giờ (tính bằng mili giây)
-    private static final long EXPIRATION_TIME = 86400000;
+    private final long EXPIRATION_TIME = expirationTime;
 
     private Key getSigningKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
